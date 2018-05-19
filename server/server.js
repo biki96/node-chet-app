f1241be96237214ddb1d4568e54bbef202514174
Milos Bijanic <bijanicmilos996@gmail.com>
2018-05-19 11:55:34 +0200

    Add geolocation suport via geolocation api

--- server/server.js
+++ server/server.js
@@ -3,39 +3,37 @@ const http = require('http');
 const express = require('express');
 const socketIO = require('socket.io');
 
-const {generateMessage} = require('./utils/message');
-publicPath = path.join(__dirname +'/../public');
+const { generateMessage, generateLocationMessage } = require('./utils/message');
+const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3000;
 var app = express();
 var server = http.createServer(app);
-
-var io = socketIO(server); //vraca web socket server, server-klijent komunikacija
+var io = socketIO(server);
 
 app.use(express.static(publicPath));
 
-io.on('connection', function (socket){ //.on slusamo dogadjaj (konekcije), socket-individualni soket
+io.on('connection', (socket) => {
   console.log('New user connected');
 
-//socket.emit - kreiranje dogadjaja
-//io.emit - vidljivo svim soketima
-
-  socket.emit('newMessage', generateMessage('Admin', 'Welcome to chat app'));
+  socket.emit('newMessage', generateMessage('Admin', 'Welcome to the chat app'));
 
   socket.broadcast.emit('newMessage', generateMessage('Admin', 'New user joined'));
 
   socket.on('createMessage', (message, callback) => {
-    console.log(message);
+    console.log('createMessage', message);
     io.emit('newMessage', generateMessage(message.from, message.text));
-    callback('This is from the server');
-
+    callback('This is from the server.');
   });
 
-  socket.on('disconnect', function (){
-    console.log('Client was disconnect');
+  socket.on('createLocationMessage', (coords) => {
+    io.emit('newLocationMessage', generateLocationMessage('Admin', coords.latitude, coords.longitude));
   });
 
+  socket.on('disconnect', () => {
+    console.log('User was disconnected');
+  });
 });
 
 server.listen(port, () => {
-  console.log(`Server is up on PORT ${port}`);
+  console.log(`Server is up on ${port}`);
 });
